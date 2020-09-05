e420ffc52211f3cd9a031723b5c6c81943d6555d
Roman <romik.rev@gmail.com>
2020-09-05 19:53:37 +0300

    sdfsd

---
+++ assets/js/table.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+
+function createBookmarks() {
+  const bookmarksTable = document.createElement("Table");
+  const linkArray = [...document.links];
+  const contentArray = linkArray.map ( (elm) => {
+    console.log("elem");
+  })
+
+
+}
+
+thi
