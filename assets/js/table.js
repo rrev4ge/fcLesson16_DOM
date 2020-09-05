041428447edb08e2854cfb6375aa72af0269e768
rrev <68103311+rrev4ge@users.noreply.github.com>
2020-09-06 01:50:01 +0300

    bugFix

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,12 +1,13 @@
 "use strict";
 
+window.addEventListener("load", createBookmarksTable, {once: true});
+
+
 function createBookmarksTable() {
   const root = document.querySelector("DL");
-  console.log("root");
   const bookmarksTable = document.createElement("table");
   root.append(bookmarksTable);
   const contentArray = [...document.links];
-  console.log(contentArray);
   contentArray.forEach((link) => {
     const tr = document.createElement("tr");
     const tdName = document.createElement("td");
@@ -20,4 +21,3 @@ function createBookmarksTable() {
 }
 
 
-window.addEventListener("click", createBookmarksTable);
